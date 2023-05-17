497e1c6be4b7203e54bb4aeed88dabc627fe9328
Erik Tkachuk <erictktk@gmail.com>
2023-05-16 23:42:08 -0400

    added more module exports

---
+++ index.js
@@ -0,0 +1,6 @@
+import * as Blendmodes from "./BlendModes.js";
+import * as Composition from "./Composition.js";
+import * as HSVClasses from "./HSVClasses.js";
+import * as HSVUtils from "./HSVUtils.js";
+
+export {Blendmodes, Composition, HSVClasses, HSVUtils};
